75e9c5c3409b2380db5e4d0e84b1bb60eccfd80f
Keannu Bernasol <noehbernasol0@gmail.com>
2023-01-30 20:48:20 +0800

    Added initial drawer structure

--- App.tsx
+++ App.tsx
@@ -3,15 +3,22 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './src/Screens/Home/Home';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import CustomDrawerContent from './src/Components/DrawerSettings/CustomDrawerContent';
 
 const Drawer = createDrawerNavigator();
-
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      drawerContent={CustomDrawerContent}
+      initialRouteName="Home">
+      <Drawer.Screen name="Home" component={Home} />
+    </Drawer.Navigator>
+  );
+}
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={Home} />
-      </Drawer.Navigator>
+      <MyDrawer />
     </NavigationContainer>
   );
 }
